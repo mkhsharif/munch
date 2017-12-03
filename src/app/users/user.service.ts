0e4c3b13d3adb8fe9a2aca98683c13f92ee1085c
Irie Grant <irieg2@gmail.com>
2017-12-03 12:58:17 -0500

    added endpoints to user service

--- src/app/users/user.service.ts
+++ src/app/users/user.service.ts
@@ -5,7 +5,53 @@ import 'rxjs/add/operator/toPromise';
 
 @Injectable()
 export class UserService {
+  private usersUrl = '/api/users';
+  private static handleError (error: any) {
+    const errMsg = (error.message) ? error.message :
+      error.status ? `${error.status} - ${error.statusText}` : 'Server error';
+    console.error(errMsg); // log to console instead
+  }
+  // noinspection JSDeprecatedSymbols
+  constructor(private http: Http) { }
 
-  constructor() { }
+  // get("/api/users")
+  getUsers(): Promise<void | User[]> {
+    return this.http.get(this.usersUrl)
+      .toPromise()
+      .then(response => response.json() as User[])
+      .catch(UserService.handleError);
+  }
 
+  // post("/api/users")
+  createUser(newUser: User): Promise<void | User> {
+    return this.http.post(this.usersUrl, newUser)
+      .toPromise()
+      .then(response => response.json() as User)
+      .catch(UserService.handleError);
+  }
+
+  // get("/api/users/:id")
+  getUser(): Promise<void | User> {
+    return this.http.get(this.usersUrl)
+      .toPromise()
+      .then(response => response.json() as User)
+      .catch(UserService.handleError);
+  }
+
+  // delete("/api/users/:id")
+  deleteUser(delUserId: String): Promise<void | String> {
+    return this.http.delete(this.usersUrl + '/' + delUserId)
+      .toPromise()
+      .then(response => response.json() as String)
+      .catch(UserService.handleError);
+  }
+
+  // put("/api/users/:id")
+  updateUser(putUser: User): Promise<void | User> {
+    const putUrl = this.usersUrl + '/' + putUser._id;
+    return this.http.put(putUrl, putUser)
+      .toPromise()
+      .then(response => response.json() as User)
+      .catch(UserService.handleError);
+  }
 }
