12ced75795acfbf4f11dc5bfa4f37da9c4c84a43
Irie Grant <irieg2@gmail.com>
2017-12-04 17:45:07 -0500

    added login and register routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,17 @@ import {PreferenceListComponent} from './munch-query/preferences/preference-list
 import {DashboardComponent} from './dashboard/dashboard.component';
 import {MunchLiveComponent} from './munch-session/munch-live/munch-live.component';
 import {MunchExitComponent} from './munch-session/munch-exit/munch-exit.component';
+import {LoginComponent} from './login/login.component';
+import {RegisterComponent} from './register/register.component';
 // TODO: add ID to munch session URLs
 const routes: Routes = [
   { path: 'munch-setup', component: PreferenceListComponent },
   { path: 'dashboard', component: DashboardComponent },
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: 'munch-session-live', component: MunchLiveComponent },
-  { path: 'munch-session-exit', component: MunchExitComponent }
+  { path: 'munch-session-exit', component: MunchExitComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent}
 ];
 
 @NgModule({
