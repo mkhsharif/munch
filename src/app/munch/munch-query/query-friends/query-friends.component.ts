863d11fdb99ca2e941824a5185391aad715d44b3
Irie Grant <irieg2@gmail.com>
2018-01-27 17:52:36 -0500

    started query friends list logic

--- src/app/munch/munch-query/query-friends/query-friends.component.ts
+++ src/app/munch/munch-query/query-friends/query-friends.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {User} from '../../../user/user';
 
 @Component({
   selector: 'app-query-friends',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QueryFriendsComponent implements OnInit {
 
+  filteredFriends: User[];
+  inputName = '';
   constructor() { }
 
   ngOnInit() {
   }
 
+  filterFriends() {
+    this.filteredFriends = [];
+    if (this.inputName !== '') {
+      this.inputName = '';
+    }
+  }
 }
