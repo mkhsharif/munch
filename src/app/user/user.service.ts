f54c08508aa1bf56d4c2c30825486c74eff0b539
irieg2 <irieg2@gmail.com>
2018-02-04 18:01:33 -0500

    updated mock user service methods

--- src/app/user/user.service.ts
+++ src/app/user/user.service.ts
@@ -3,15 +3,39 @@ import { User } from './user';
 import 'rxjs/add/operator/toPromise';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs/Observable';
+import 'rxjs/add/observable/of';
 
 @Injectable()
 export class UserService {
   private usersUrl = '/api/users';
+  private mock1: User = {
+    userName: 'a',
+    firstName: 'a',
+    lastName: 'a',
+    password: 'a',
+    email: 'a',
+    phone: 'a',
+    points: 0,
+    friends: [],
+    shoutouts: []
+};
+  private mock2: User = {
+    userName: 'b',
+    firstName: 'b',
+    lastName: 'b',
+    password: 'b',
+    email: 'b',
+    phone: 'b',
+    points: 0,
+    friends: [],
+    shoutouts: [],
+  };
   private static handleError (error: any) {
     const errMsg = (error.message) ? error.message :
       error.status ? `${error.status} - ${error.statusText}` : 'Server error';
     console.error(errMsg); // log to console instead
   }
+
   // noinspection JSDeprecatedSymbols
   constructor(private http: HttpClient) { }
 
@@ -26,8 +50,20 @@ export class UserService {
   }
 
   // get("/api/users/:id")
-  getUser(getUserId: String): Observable<User> {
+  getUser(getUserId: string): Observable<User> {
+    if (getUserId === this.mock1._id) {
+      return Observable.of(this.mock1);
+    }
+
+    if (getUserId === this.mock2._id) {
+      return Observable.of(this.mock2);
+    }
+
     return this.http.get<User>(this.usersUrl  + '/' + getUserId);
+  }
+
+  getMockUsers(): Observable<User[]> {
+    return Observable.of([this.mock1, this.mock2]);
   }
 
   // delete("/api/users/:id")
