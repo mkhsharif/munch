fb751ef13b262a731f1add87fffe81b35c1c53d3
irieg2 <irieg2@gmail.com>
2018-02-04 17:47:34 -0500

    implemented getSession and added _id to session

--- src/app/munch/munch-session/munch-session.ts
+++ src/app/munch/munch-session/munch-session.ts
@@ -1,6 +1,7 @@
 import {User} from '../../user/user';
 
 export class MunchSession {
+  _id?: string;
   live: boolean;
   users: string[];
 }
--- src/app/munch/munch-session/session.service.ts
+++ src/app/munch/munch-session/session.service.ts
@@ -4,12 +4,15 @@ import {MunchSession} from './munch-session';
 @Injectable()
 export class SessionService {
   private mockMunchSession: MunchSession = {
+    _id: 'fakeSession1',
     live: false,
     users: ['mockUser1', 'mockUser2'],
   };
   constructor() { }
 
-
-
-
+  getSession(getSessionId: string) {
+    if (getSessionId === this.mockMunchSession._id) {
+      return this.mockMunchSession;
+    }
+  }
 }
