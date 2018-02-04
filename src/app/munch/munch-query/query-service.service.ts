09d7c4cd152fcb56508cd9fbd64d78b0e25fb31a
Acespace <alston.clark17@gmail.com>
2018-02-04 16:20:26 -0500

    adding query dummy data

--- src/app/munch/munch-query/query-service.service.ts
+++ src/app/munch/munch-query/query-service.service.ts
@@ -1,8 +1,26 @@
 import { Injectable } from '@angular/core';
+import { Query } from './query';
+import {DIETPREFERENCE, GENDERPREFERENCE, INTERESTSPREFERENCE, LOCATIONPREFERENCE} from './preferences/preference-list';
+import {User} from '../../user/user';
 
 @Injectable()
 export class QueryServiceService {
 
   constructor() { }
+
+  getQueries() {
+    const qs: Query[] = [];
+    const a: Query = {
+      user: '',
+      status: '',
+      locationPreference: 'Annex',
+      dietPreference: 'Vegan',
+      genderPreference: 'Female',
+      interestsPreference: 'Music'
+    };
+    qs.push(a);
+    return qs;
+
+  }
 
 }
