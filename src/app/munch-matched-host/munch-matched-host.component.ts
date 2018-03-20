ebd63b7e6c24e8152af66288cd93d0735b668047
Irie Grant <irieg2@gmail.com>
2018-03-19 22:33:57 -0400

    created a mock session object to display data in page

--- src/app/munch-matched-host/munch-matched-host.component.ts
+++ src/app/munch-matched-host/munch-matched-host.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {MunchSession} from '../_models/munch-session';
 
 @Component({
   selector: 'app-munch-matched-host',
@@ -7,6 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MunchMatchedHostComponent implements OnInit {
 
+  // TODO: update this to use a service
+  munchSession: MunchSession = {
+    _id: 's1',
+    host_id: 'u1',
+    user_ids: ['u1', 'u2'],
+    location_id: 'l1',
+    pending: true,
+    active: true
+  };
   constructor() { }
 
   ngOnInit() {
