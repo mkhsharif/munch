c39ae3de56e9efb3e900217b6599295473daf460
Irie Grant <irieg2@gmail.com>
2018-03-19 22:46:24 -0400

    filled in mock data

--- src/app/munch-matched-host/munch-matched-host.component.ts
+++ src/app/munch-matched-host/munch-matched-host.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {MunchSession} from '../_models/munch-session';
+import {User} from '../_models/user';
+import {Location} from '../_models/location';
 
 @Component({
   selector: 'app-munch-matched-host',
@@ -8,18 +10,61 @@ import {MunchSession} from '../_models/munch-session';
 })
 export class MunchMatchedHostComponent implements OnInit {
 
-  // TODO: update this to use a service
-  munchSession: MunchSession = {
-    _id: 's1',
-    host_id: 'u1',
-    user_ids: ['u1', 'u2'],
-    location_id: 'l1',
-    pending: true,
-    active: true
-  };
+  // TODO: update objects to use services and real data
+  munchSession: MunchSession;
+  currentUser: User;
+  matchedUser: User;
+  location: Location;
   constructor() { }
 
   ngOnInit() {
+    this.munchSession = {
+      _id: 's1',
+      host_id: 'u1',
+      user_ids: ['u1', 'u2'],
+      location_id: 'l1',
+      pending: true,
+      active: true
+    };
+
+    this.currentUser = {
+      _id: 'u1',
+      firstName: 'current',
+      lastName: 'user',
+      userName: 'currentUser',
+      email: '',
+      phone: '',
+      points: 0,
+      password: '',
+      friend_ids: [],
+      shoutout_ids: [],
+      avatarUrl: '',
+      interests: [],
+      diet_id: ''
+    };
+
+    this.matchedUser = {
+      _id: 'u2',
+      firstName: 'matched',
+      lastName: 'user',
+      userName: 'matchedUser',
+      email: '',
+      phone: '',
+      points: 0,
+      password: '',
+      friend_ids: [],
+      shoutout_ids: [],
+      avatarUrl: '',
+      interests: [],
+      diet_id: ''
+    };
+
+    this.location = {
+      _id: 'l1',
+      name: 'Blackburn',
+      longitude: '1',
+      latitude: '2'
+    };
   }
 
 }
