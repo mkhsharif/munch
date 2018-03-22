c887ecd867bb6e084d10d22c089010f62e563cb9
irieg2 <irieg2@gmail.com>
2018-03-22 11:06:12 -0400

    cleaning up deprecated components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {DashboardComponent} from './dashboard/dashboard.component';
-import {MunchLiveComponent} from './munch/munch-live/munch-live.component';
-import {MunchExitComponent} from './munch/munch-exit/munch-exit.component';
 import {LoginComponent} from './user/login/login.component';
 import {RegisterComponent} from './user/register/register.component';
 import {ShoutOutDetailComponent} from './dashboard/shout-outs/shout-out-detail/shout-out-detail.component';
@@ -10,8 +8,6 @@ import {ShoutOutListComponent} from './dashboard/shout-outs/shout-out-list/shout
 import {ShoutOutCreateComponent} from './dashboard/shout-outs/shout-out-create/shout-out-create.component';
 import {MunchQueryComponent} from './munch/munch-query/munch-query.component';
 import {UserSettingsComponent} from './user/user-settings/user-settings.component';
-import {MunchSearchComponent} from './munch/munch-search/munch-search.component';
-import {EndQueryGuard} from './_guards/end-query.guard';
 import {CanDeactivateGuard} from './_guards/can-deactivate-guard.service';
 import {UserComponent} from './user/user.component';
 import {HomepageComponent} from './homepage/homepage.component';
@@ -32,9 +28,6 @@ const routes: Routes = [
   { path: 'shoutouts/:id', component: ShoutOutDetailComponent},
   { path: 'shoutouts', component: ShoutOutListComponent},
   { path: 'settings', component: UserSettingsComponent, canActivate: []},
-  { path: 'munch/search/:id', component: MunchSearchComponent, canActivate: [], canDeactivate: [CanDeactivateGuard]},
-  { path: 'munch/session/:id', component: MunchLiveComponent, canDeactivate: [CanDeactivateGuard]},
-  { path: 'munch/exit/:id', component: MunchExitComponent, canActivate: [], },
   { path: 'user', component: UserComponent },
   { path: 'homepage', component: HomepageComponent},
   { path: 'profile', component: ProfileComponent},
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,8 +11,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { PreferenceDetailComponent } from './munch/munch-query/preferences/preference-list/preference-detail/preference-detail.component';
 import { PreferenceListComponent } from './munch/munch-query/preferences/preference-list/preference-list.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
-import { MunchLiveComponent } from './munch/munch-live/munch-live.component';
-import { MunchExitComponent } from './munch/munch-exit/munch-exit.component';
 import { ShoutOutsComponent } from './dashboard/shout-outs/shout-outs.component';
 import { ShoutOutListComponent } from './dashboard/shout-outs/shout-out-list/shout-out-list.component';
 import { ShoutOutDetailComponent } from './dashboard/shout-outs/shout-out-detail/shout-out-detail.component';
@@ -25,7 +23,6 @@ import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
 import { QueryFriendsComponent } from './munch/munch-query/query-friends/query-friends.component';
 import { UserComponent } from './user/user.component';
-import { MunchComponent } from './munch/munch.component';
 import { RewardsComponent } from './rewards/rewards.component';
 import { RewardDetailComponent } from './rewards/reward-detail/reward-detail.component';
 import { RewardListComponent } from './rewards/reward-list/reward-list.component';
@@ -33,11 +30,8 @@ import { UserSessionsListComponent } from './user/user-sessions-list/user-sessio
 import { UserFriendsListComponent } from './user/user-friends-list/user-friends-list.component';
 import { UserSettingsComponent } from './user/user-settings/user-settings.component';
 import {AuthGuard} from './_guards/auth.guard';
-import { MunchSessionComponent } from './munch/munch-session/munch-session.component';
-import { MunchSearchComponent } from './munch/munch-search/munch-search.component';
 import {MunchRequestService} from './_services/munch-request.service';
 import {SessionService} from './_services/munch-session.service';
-import { MunchLoadingComponent } from './munch/munch-loading/munch-loading.component';
 import {EndQueryGuard} from './_guards/end-query.guard';
 import {HashLocationStrategy, LocationStrategy} from '@angular/common';
 import {CanDeactivateGuard} from './_guards/can-deactivate-guard.service';
@@ -62,8 +56,6 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     PreferenceDetailComponent,
     PreferenceListComponent,
     DashboardComponent,
-    MunchLiveComponent,
-    MunchExitComponent,
     ShoutOutsComponent,
     ShoutOutListComponent,
     ShoutOutDetailComponent,
@@ -72,21 +64,17 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     NavigationBarComponent,
     QueryFriendsComponent,
     UserComponent,
-    MunchComponent,
     RewardsComponent,
     RewardDetailComponent,
     RewardListComponent,
     UserSessionsListComponent,
     UserFriendsListComponent,
     UserSettingsComponent,
-    MunchSessionComponent,
-    MunchSearchComponent,
-    MunchLoadingComponent,
     HomepageComponent,
     ProfileComponent,
     MusicAffinityComponent,
     FoodafComponent,
-    InterestsComponent
+    InterestsComponent,
     FoodafComponent,
     WaitingPageComponent
   ],
