638ecdfcc1bb50b67f00f918e931624ce16857b7
Acespace <alston.clark17@gmail.com>
2018-03-27 11:33:18 -0400

    first commit for branch

--- src/app/music-affinity/music-affinity.component.ts
+++ src/app/music-affinity/music-affinity.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {UserInterest} from '../_models/user-interest';
+import {INTERESTSLIST, Preference} from '../_models/preference';
 
 @Component({
   selector: 'app-music-affinity',
@@ -6,10 +8,47 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./music-affinity.component.css']
 })
 export class MusicAffinityComponent implements OnInit {
-
-  constructor() { }
-
+  constructor() {
+  }
   ngOnInit() {
   }
+  export class User{
+  _id?: string;
+  userName: string;
+  firstName: string;
+  lastName: string;
+  password: string;
+  email: string;
+  phone: string;
+  points: number;
+  friend_ids: string[];
+  shoutout_ids: string[];
+  avatarUrl: string;
+  interests: UserInterest[];
+  diet_id: string;
+}
 
+
+  export class Kanye {
+  like?: string;
+  }
+  export class Noname {
+  like?: string;
+  }
+  export class RobertGlasper {
+  like?: string;
+  }
+  export class Jazz {
+  like?: string;
+  }
+  export class Mozart {
+  like?: string;
+  }
+  function submit() {
+    console.log(Kanye);
+    console.log(Noname);
+    console.log(RobertGlasper);
+    console.log(Jazz);
+    console.log(Mozart);
+  }
 }
