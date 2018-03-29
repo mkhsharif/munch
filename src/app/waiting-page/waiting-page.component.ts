cb90d870473822146c6c16f70da08dc989d2ee66
irieg2 <irieg2@gmail.com>
2018-03-29 11:29:48 -0400

    get request from url and also list of all requests

--- src/app/waiting-page/waiting-page.component.ts
+++ src/app/waiting-page/waiting-page.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {MunchRequest} from '../_models/munch-request';
+import {Observable} from 'rxjs/Observable';
+import {ActivatedRoute} from '@angular/router';
+import {MunchRequestService} from '../_services/munch-request.service';
 
 @Component({
   selector: 'app-waiting-page',
@@ -9,9 +12,32 @@ import {MunchRequest} from '../_models/munch-request';
 export class WaitingPageComponent implements OnInit {
 
   request: MunchRequest;
-  constructor() { }
+  requests: MunchRequest[];
+  constructor(
+    private route: ActivatedRoute,
+    private requestService: MunchRequestService
+  ) { }
 
   ngOnInit() {
+    this.getRequest().subscribe();
+    this.getRequests().subscribe();
+  }
+
+  getRequest(): Observable<MunchRequest> {
+    const id = this.route.snapshot.paramMap.get('id');
+    return this.requestService.getRequest(id)
+      .map((request: MunchRequest) => {
+        this.request = request;
+        return this.request;
+    });
+  }
+
+  getRequests(): Observable<MunchRequest[]> {
+    return this.requestService.getRequests()
+      .map((requests: MunchRequest[]) => {
+        this.requests = requests;
+        return this.requests;
+      });
   }
 
 }
