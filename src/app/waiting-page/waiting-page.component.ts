b146396262c9674458bb9bce10c57b5e6d87c946
irieg2 <irieg2@gmail.com>
2018-03-29 10:41:10 -0400

    started new branch, defined request object

--- src/app/waiting-page/waiting-page.component.ts
+++ src/app/waiting-page/waiting-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {MunchRequest} from '../_models/munch-request';
 
 @Component({
   selector: 'app-waiting-page',
@@ -7,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WaitingPageComponent implements OnInit {
 
+  request: MunchRequest;
   constructor() { }
 
   ngOnInit() {
