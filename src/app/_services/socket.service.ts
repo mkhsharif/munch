ccdf7cd44b529faadbdad842b4650268f433ac5c
irieg2 <irieg2@gmail.com>
2018-03-31 00:41:22 -0400

    filled in creating match methods for socket service

--- src/app/_services/socket.service.ts
+++ src/app/_services/socket.service.ts
@@ -1,8 +1,27 @@
 import { Injectable } from '@angular/core';
+import * as io from 'socket.io-client';
+import {Observable} from 'rxjs/Observable';
+import {MunchSession} from '../_models/munch-session';
 
 @Injectable()
 export class SocketService {
+  private socket: SocketIOClient.Socket;
 
   constructor() { }
+
+  initSocket(): void {
+    this.socket = io();
+  }
+
+  createMatch(session: MunchSession): void {
+    this.socket.emit('create-match', session._id);
+  }
+
+  onNewMatch(): Observable<string> {
+    return new Observable<string>(
+      observer => {
+        this.socket.on('new-match', (data: string) => observer.next(data));
+    });
+  }
 
 }
