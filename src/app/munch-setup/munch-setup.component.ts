80a248e6f086c75b23c866dca6b6d459c53d0b28
KezziaCarter <33240092+KezziaCarter@users.noreply.github.com>
2018-03-31 16:56:06 -0400

    Next button greys out when we reach the munch button page

--- src/app/munch-setup/munch-setup.component.ts
+++ src/app/munch-setup/munch-setup.component.ts
@@ -17,6 +17,7 @@ import {MunchRequestService} from '../_services/munch-request.service';
 })
 export class MunchSetupComponent implements OnInit {
   state: number;
+  isStateOne: boolean;
   locations: MunchLocation[];
   diets: Diet[] = DIETS;
   request: MunchRequest;
@@ -26,6 +27,7 @@ export class MunchSetupComponent implements OnInit {
     private userService: UserService,
     private requestService: MunchRequestService) {
     this.state = 1;
+    this.isStateOne = true;
   }
 
   ngOnInit() {
@@ -51,28 +53,56 @@ export class MunchSetupComponent implements OnInit {
   }
 
   incrementState(): void {
-    var stateString = this.state.toString();
-    var currentStateElements = document.getElementsByClassName("state-"+stateString);
-    var currentStateElement = (currentStateElements[0] as HTMLElement);
-    var newStateInt = this.state + 1;
-    var newStateClassName = "state-" + newStateInt;
-    var newStateElements = document.getElementsByClassName(newStateClassName);
-    var newStateElement = newStateElements[0];
-    currentStateElement.classList.add("inactive");
-    newStateElement.classList.remove("inactive");
-    this.state = newStateInt;
+    if (this.state != 1) {
+      this.isStateOne = false;
+    }
+    var nextButtonElements = document.getElementsByClassName("next-button");
+    var nextButtonElement = (nextButtonElements[0] as HTMLElement);
+    // we move on to the munch match after state 5
+    if (this.state != 5) {
+      // if the next button is already greyed out, undo it
+      if (nextButtonElement.classList.contains("greyed-out")) {
+        nextButtonElement.classList.remove("greyed-out");
+      }
+      var stateString = this.state.toString();
+      var currentStateElements = document.getElementsByClassName("state-"+stateString);
+      var currentStateElement = (currentStateElements[0] as HTMLElement);
+      var newStateInt = this.state + 1;
+      var newStateClassName = "state-" + newStateInt;
+      var newStateElements = document.getElementsByClassName(newStateClassName);
+      var newStateElement = newStateElements[0];
+      currentStateElement.classList.add("inactive");
+      newStateElement.classList.remove("inactive");
+      this.state = newStateInt;
+    }
+    else {
+      nextButtonElement.classList.add("greyed-out");
+    }
   }
 
   decrementState(): void {
-    var stateString = this.state.toString();
-    var currentStateElements = document.getElementsByClassName("state-"+stateString);
-    var currentStateElement = (currentStateElements[0] as HTMLElement);
-    var newStateInt = this.state - 1;
-    var newStateClassName = "state-" + newStateInt;
-    var newStateElements = document.getElementsByClassName(newStateClassName);
-    var newStateElement = newStateElements[0];
-    currentStateElement.classList.add("inactive");
-    newStateElement.classList.remove("inactive");
-    this.state = newStateInt;
+    if (this.state != 1) {
+      this.isStateOne = false;
+    }
+    // back next button shouldn't be grey if state is being decremented
+    var nextButtonElements = document.getElementsByClassName("next-button");
+    var nextButtonElement = (nextButtonElements[0] as HTMLElement);
+      // if the next button is already greyed out, undo it
+    if (nextButtonElement.classList.contains("greyed-out")) {
+      nextButtonElement.classList.remove("greyed-out");
+    }
+    //we move back to the dashbord before state 1
+    if (this.state != 1) {
+      var stateString = this.state.toString();
+      var currentStateElements = document.getElementsByClassName("state-"+stateString);
+      var currentStateElement = (currentStateElements[0] as HTMLElement);
+      var newStateInt = this.state - 1;
+      var newStateClassName = "state-" + newStateInt;
+      var newStateElements = document.getElementsByClassName(newStateClassName);
+      var newStateElement = newStateElements[0];
+      currentStateElement.classList.add("inactive");
+      newStateElement.classList.remove("inactive");
+      this.state = newStateInt;
+    }
   }
 }
