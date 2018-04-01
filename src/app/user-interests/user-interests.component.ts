f0aa14d73c33e272fc6686086d26fe0d927782d9
Irie Grant <irieg2@gmail.com>
2018-04-01 11:16:24 -0400

    populate interestSelections based on user interests, change checked property of checkbox to reflect

--- src/app/user-interests/user-interests.component.ts
+++ src/app/user-interests/user-interests.component.ts
@@ -18,14 +18,25 @@ export class UserInterestsComponent implements OnInit {
   interests: Interest[] = INTERESTS;
   interestSelections: InterestSelection[] = [];
 
-
   constructor(
     private userService: UserService,
     private interestsService: InterestService) { }
 
   ngOnInit() {
-    this.getUser().subscribe();
-    this.getInterests().subscribe();
+    this.getInterests().flatMap(() => {
+      return this.getUser();
+    }).subscribe((user: User) => {
+      const userInterestIds: string[] = [];
+      for (const userInterest of user.interests) {
+        userInterestIds.push(userInterest.interest_id);
+      }
+      for (const interest of this.interests) {
+        const select = userInterestIds.includes(interest._id);
+        this.interestSelections.push(
+          {interest_id: interest._id, selected: select}
+        );
+      }
+    });
   }
 
   getUser(): Observable<User> {
