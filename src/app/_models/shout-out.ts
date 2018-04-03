85d98680647bab6ffb3249fe539f610a52dd2929
irieg2 <irieg2@gmail.com>
2018-04-03 11:00:47 -0400

    mass refactor, removing anything that is not for the mvp

--- src/app/_models/shout-out.ts
+++
@@ -1,6 +0,0 @@
-export class ShoutOut {
-  _id?: string;
-  user: string;
-  message: string;
-  link?: string;
-}
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import {DashboardComponent} from './dashboard/dashboard.component';
-import {LoginComponent} from './user/login/login.component';
-import {RegisterComponent} from './user/register/register.component';
-import {ShoutOutDetailComponent} from './dashboard/shout-outs/shout-out-detail/shout-out-detail.component';
-import {ShoutOutListComponent} from './dashboard/shout-outs/shout-out-list/shout-out-list.component';
-import {ShoutOutCreateComponent} from './dashboard/shout-outs/shout-out-create/shout-out-create.component';
-import {MunchQueryComponent} from './munch/munch-query/munch-query.component';
+import {LoginComponent} from './login/login.component';
+import {RegisterComponent} from './register/register.component';
 import {CanDeactivateGuard} from './_guards/can-deactivate-guard.service';
 import {HomepageComponent} from './homepage/homepage.component';
 import {ProfileComponent} from './profile/profile.component';
@@ -15,26 +10,24 @@ import {MunchMatchedComponent} from './munch-matched/munch-matched.component';
 import {UserInterestsComponent} from './user-interests/user-interests.component';
 import {MunchActiveComponent} from './munch-active/munch-active.component';
 import {LandingPageComponent} from './landing-page/landing-page.component';
+import {MunchSetupComponent} from './munch-setup/munch-setup.component';
 
 
 // TODO: add ID to munch session URLs
 
 const routes: Routes = [
-  { path: 'munch/setup', component: MunchQueryComponent, canActivate: [] },
-  { path: 'dashboard', component: DashboardComponent },
+  { path: 'home', component: HomepageComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
-  { path: 'shoutouts/create', component: ShoutOutCreateComponent, canActivate: []},
-  { path: 'shoutouts/:id', component: ShoutOutDetailComponent},
-  { path: 'shoutouts', component: ShoutOutListComponent},
   { path: 'profile', component: ProfileComponent},
   { path: 'profile/interests', component: UserInterestsComponent},
   { path: 'homepage', component: HomepageComponent},
+  { path: 'munch/setup', component: MunchSetupComponent},
   { path: 'munch/waiting/:id', component: WaitingPageComponent},
   { path: 'munch/match/:id', component: MunchMatchedComponent},
   { path: 'munch/active/:id', component: MunchActiveComponent},
   { path: 'landing-page', component: LandingPageComponent},
-  { path: '', redirectTo: '/dashboard', pathMatch: 'full' }
+  { path: '', redirectTo: '/homepage', pathMatch: 'full' }
 ];
 
 @NgModule({
