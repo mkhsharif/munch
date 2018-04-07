183dbe9d619cb21a0d45bea908086b84bec186a7
mkhs <mkh_sharif@aol.com>
2018-04-07 17:45:21 -0400

    diet page update attached to user object

--- src/app/user-diets/user-diets.component.ts
+++ src/app/user-diets/user-diets.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import {Diets} from '../_models/diets';
+import {User} from '../_models/user';
+import {Observable} from 'rxjs/Observable';
+import {Interest} from '../_models/interest';
+import {UserInterest} from '../_models/user-interest';
+import {UserService} from '../_services/user.service';
 
 @Component({
   selector: 'app-user-diets',
@@ -7,12 +12,29 @@ import {Diets} from '../_models/diets';
   styleUrls: ['./user-diets.component.css']
 })
 export class UserDietsComponent implements OnInit {
+
+  user: User;
   diets: Diets[] = [Diets.ANY, Diets.VEGETARIAN, Diets.HALAL, Diets.PESCATARIAN, Diets.VEGAN];
   selectedDiet: Diets;
 
-  constructor() { }
+  constructor(
+    private userService: UserService) { }
 
   ngOnInit() {
+    this.getUser().subscribe();
   }
 
+  getUser(): Observable<User> {
+   return this.userService.getUser('u1')
+     .map((user: User) => {
+       this.user = user;
+       return this.user;
+     });
+  }
+
+  updateDiet(): void {
+    this.user.diet = this.selectedDiet;
+    console.log('User updated diet: ' + this.user.diet);
+  }
 }
+
