17182cf11d464783e04df8b98ee435383850404a
mkhs <mkh_sharif@aol.com>
2018-04-10 19:16:50 -0400

    diet settings moved to settings

--- src/app/user-settings/user-settings.component.ts
+++ src/app/user-settings/user-settings.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import {AuthenticationService} from '../_services/authentication.service';
+import {Diets} from '../_models/diets';
+import {User} from '../_models/user';
+import {Observable} from 'rxjs/Observable';
+import {UserService} from '../_services/user.service';
 
 
 @Component({
@@ -9,14 +13,43 @@ import {AuthenticationService} from '../_services/authentication.service';
 })
 export class UserSettingsComponent implements OnInit {
 
-  constructor(public authService: AuthenticationService) {
-  }
+  user: User;
+  diets: Diets[] = [Diets.ANY, Diets.VEGETARIAN, Diets.HALAL, Diets.PESCATARIAN, Diets.VEGAN];
+  selectedDiet: Diets;
+
+
+  constructor(public authService: AuthenticationService,
+              public userService: UserService) {}
 
   ngOnInit() {
     console.log('settings');
+    this.getUser().subscribe();
   }
 
   logout() {
     this.authService.logout();
   }
+
+  getUser(): Observable<User> {
+    const id: string = this.userService.getCurrentUser()._id;
+    return this.userService.getUser(id)
+      .map((user: User) => {
+        this.user = user;
+        return this.user;
+      });
+  }
+
+  updateDiet(): void {
+    console.log('Previous diet:' + this.user.diet);
+    this.user.diet = this.selectedDiet;
+    this.userService.updateUser(this.user).subscribe(() => {
+       console.log('User updated diet: ' + this.user.diet);
+      });
+    }
 }
+
+
+
+
+
+
