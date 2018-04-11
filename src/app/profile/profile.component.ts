e17420b895c6989cec3c19bbe9b9c1edef1c1dd5
mkhs <mkh_sharif@aol.com>
2018-04-10 22:30:34 -0400

    profile page updated

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,5 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import {AuthenticationService} from '../_services/authentication.service';
+import {Interest} from '../_models/interest';
+import {INTERESTS} from '../_models/interest-list';
+import {User} from '../_models/user';
+import {UserService} from '../_services/user.service';
+import {Observable} from 'rxjs/Observable';
+import {InterestService} from '../_services/interest.service';
+import {UserInterest} from '../_models/user-interest';
+
+
+class InterestSelection {
+  interest_id: string;
+  selected: boolean;
+}
 
 @Component({
   selector: 'app-profile',
@@ -8,13 +21,64 @@ import {AuthenticationService} from '../_services/authentication.service';
 })
 export class ProfileComponent implements OnInit {
 
-  constructor(private authService: AuthenticationService) { }
+  user: User;
+  interests: Interest[] = INTERESTS;
+  interestSelections: InterestSelection[] = [];
+
+  constructor(private authService: AuthenticationService,
+              private interestService: InterestService,
+              private  userService: UserService) { }
 
   ngOnInit() {
+    this.getInterests().flatMap(() => {
+      return this.getUser();
+    }).subscribe((user: User) => {
+      const userInterestIds: string[] = [];
+      for (const userInterest of user.interests) {
+        userInterestIds.push(userInterest.interest_id);
+      }
+      for (const interest of this.interests) {
+        const select = userInterestIds.includes(interest._id);
+        this.interestSelections.push(
+          {interest_id: interest._id, selected: select}
+        );
+      }
+    });
   }
 
-  logout() {
-    this.authService.logout();
+  getUser(): Observable<User> {
+    const id = this.userService.getCurrentUser()._id;
+    return this.userService.getUser(id)
+      .map((user: User) => {
+        this.user = user;
+        return this.user;
+      });
+  }
+
+  getInterests(): Observable<Interest[]> {
+    return this.interestService.getInterests()
+      .map((interests: Interest[]) => {
+        this.interests = interests;
+        return this.interests;
+      });
+  }
+
+  updateInterests(): void {
+    const user_interests: UserInterest[] = [];
+    for (const interest of this.interestSelections) {
+      if (interest.selected === true) {
+        user_interests.push({interest_id: interest.interest_id, weight: 1});
+      }
+    }
+    this.user.interests = user_interests;
+    this.userService.updateUser(this.user)
+      .subscribe((user: User) => {
+          console.log('Updated interests for ' + user.userName);
+        });
+  }
+
+  toggleInterest(index, event) {
+    this.interestSelections[index].selected = event.checked;
   }
 
 }
